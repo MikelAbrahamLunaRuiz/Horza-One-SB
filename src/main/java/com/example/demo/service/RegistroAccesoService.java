fa70e6d06d3efd9ca92266cf8733d965b0ea3dda
MikelAbrahamLunaRuiz <mlunar2301@alumno.ipn.mx>
2026-04-18 08:38:27 -0600

    agregar application.properties con variables de entorno 2

--- src/main/java/com/example/demo/service/RegistroAccesoService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.dto.RegistroAccesoRequest;
-import com.example.demo.dto.RegistroAccesoResponse;
-
-public interface RegistroAccesoService {
-    RegistroAccesoResponse registrarAcceso(RegistroAccesoRequest request);
-}
