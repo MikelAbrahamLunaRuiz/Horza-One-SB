1ccd857e73d1afb0b6ae1fbce27a196d815a04cc
MikelAbrahamLunaRuiz <mlunar2301@alumno.ipn.mx>
2026-04-19 09:10:48 -0600

    Login completo 5

--- src/main/java/com/example/demo/dto/LoginResponse.java
+++ src/main/java/com/example/demo/dto/LoginResponse.java
@@ -3,11 +3,9 @@ package com.example.demo.dto;
 
 import java.util.List;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 
 public class LoginResponse {
     private Boolean success;
